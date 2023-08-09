71de14f5f3d2ca77fef4b47c584dc454ce6c0cbe
Matheus Moreira <132686489+mathssmoreira@users.noreply.github.com>
2023-08-09 18:22:01 -0300

    Update calculadoraProfessor.java

--- calculadoraProfessor.java
+++ calculadoraProfessor.java
@@ -4,10 +4,12 @@ public class calculadoraProfessor {
 
 	float pi = 3.14f;
 	float raio = 5.3f;
-
+		
+	// Calculando a área do circulo
 	float areaCirculo = (raio * 2) * pi;
 	System.out.println("Area do circulo: " + areaCirculo);
-	
+		
+	// Calculando a área do circulo usando CASTING para forçar conversão de float para inteiro
 	int conversaoTipoAreaCirculo = (int) areaCirculo;
 	System.out.println("Area do circulo convertida para o tipo int : " + conversaoTipoAreaCirculo );
 	
